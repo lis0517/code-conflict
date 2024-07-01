8aeb69887d4b7ae20403ee9a83efd26e22e82846
lis0517 <01034895017a@gmail.com>
2024-07-01 15:52:53 +0900

    update MemberController
    회원 조회 기능 추가 중

--- src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
+++ src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
@@ -1,4 +1,18 @@
 package kr.sparta.practiceconflict.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.sparta.practiceconflict.entity.Member;
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
+
+	@GetMapping("/{id}")
+	public Member getMember(@PathVariable Long id) {
+		return null;
+	}
 }
