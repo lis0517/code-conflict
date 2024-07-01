5387227e9229dfe2d9f9fd9e27323391bb46b192
lis0517 <01034895017a@gmail.com>
2024-07-01 16:05:25 +0900

    fix conflict

--- src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
+++ src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
@@ -1,6 +1,8 @@
 package kr.sparta.practiceconflict.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +17,10 @@ public class MemberController {
 	@PostMapping
 	public ResponseEntity<?> addMember(@RequestBody Member member) {
 		return ResponseEntity.ok(null);
+	}
+
+	@GetMapping("/{id}")
+	public Member getMember(@PathVariable Long id) {
+		return null;
 	}
 }
