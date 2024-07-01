b0e08719829bd5589763ed1de1a3faa97c0e7c95
lis0517 <01034895017a@gmail.com>
2024-07-01 15:59:39 +0900

    update MemberController
    멤버 추가 함수 구현 중

--- src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
+++ src/main/java/kr/sparta/practiceconflict/controller/MemberController.java
@@ -1,4 +1,19 @@
 package kr.sparta.practiceconflict.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.sparta.practiceconflict.entity.Member;
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
+
+	@PostMapping
+	public ResponseEntity<?> addMember(@RequestBody Member member) {
+		return ResponseEntity.ok(null);
+	}
 }
